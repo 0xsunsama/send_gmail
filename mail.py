9d147a09279aa7169e35a92e6a81f8c12dba7c01
孙浩然 <sunhr@knownsec.com>
2017-02-13 20:58:53 +0800

    sendgmail

---
+++ mail.py
@@ -0,0 +1,20 @@
+import smtplib
+from httplib2 import socks
+import socket
+socket.socket = socks.socksocket
+socks.setdefaultproxy(socks.PROXY_TYPE_SOCKS5,"127.0.0.1",1080)
+# The below code never changes, though obviously those variables need values.
+session = smtplib.SMTP('smtp.gmail.com', 587)
+session.ehlo()
+session.starttls()
+session.login("sunsama1995@gmail.com", "vlsbexgtjuvclxsu")
+ 
+headers = "\r\n".join(["from: " + "sunsama1995@gmail.com",
+            "subject: " + "hello"
+            "to: " + "691367132@qq.com",
+            "mime-version: 1.0",
+            "content-type: text/html"])
+ 
+# body_of_email can be plaintext or html!          
+content = headers + "\r\n\r\n" + "hello"
+session.sendmail("sunsama1995@gmail.com", "691367132@qq.com", content)
